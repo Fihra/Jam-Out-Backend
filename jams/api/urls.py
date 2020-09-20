8c98fdc2397e7daf65df209ee701ac98eefc7f30
Fihra <fabrofabian@gmail.com>
2020-09-20 02:46:05 -0700

    Added crud using Viewset Routers

--- jams/api/urls.py
+++ jams/api/urls.py
@@ -1,8 +1,24 @@
-from django.urls import path
+from jams.api.views import JamViewSet
+from rest_framework.routers import DefaultRouter
 
-from .views import JamListView, JamDetailView
+router = DefaultRouter()
+router.register(r'', JamViewSet, basename='jams')
+urlpatterns = router.urls
 
-urlpatterns = [
-    path('', JamListView.as_view()),
-    path('<pk>', JamDetailView.as_view()),
-]
+# from django.urls import path
+
+# from .views import (
+#     JamListView, 
+#     JamDetailView,
+#     JamCreateView,
+#     JamUpdateView,
+#     JamDeleteView
+# )
+
+# urlpatterns = [
+#     path('', JamListView.as_view()),
+#     path('create/', JamCreateView.as_view()),
+#     path('<pk>', JamDetailView.as_view()),
+#     path('<pk>/update/', JamUpdateView.as_view()),
+#     path('<pk>/delete/', JamDeleteView.as_view()),
+# ]
--- jams/api/views.py
+++ jams/api/views.py
@@ -1,14 +1,36 @@
-#Go to generic Views in documentaiton
-#Concrete View Classes
-
-from rest_framework.generics import ListAPIView, RetrieveAPIView
 from jams.models import Jam
 from .serializers import JamSerializer
 
-class JamListView(ListAPIView):
-    queryset = Jam.objects.all()
-    serializer_class = JamSerializer
+from rest_framework import viewsets
 
-class JamDetailView(RetrieveAPIView):
-    queryset = Jam.objects.all()
+class JamViewSet(viewsets.ModelViewSet):
     serializer_class = JamSerializer
+    queryset = Jam.objects.all()
+
+# from rest_framework.generics import (
+#     ListAPIView, 
+#     RetrieveAPIView,
+#     CreateAPIView,
+#     DestroyAPIView,
+#     UpdateAPIView
+# )
+
+# class JamListView(ListAPIView):
+#     queryset = Jam.objects.all()
+#     serializer_class = JamSerializer
+
+# class JamDetailView(RetrieveAPIView):
+#     queryset = Jam.objects.all()
+#     serializer_class = JamSerializer
+
+# class JamCreateView(CreateAPIView):
+#     queryset = Jam.objects.all()
+#     serializer_class = JamSerializer
+
+# class JamUpdateView(UpdateAPIView):
+#     queryset = Jam.objects.all()
+#     serializer_class = JamSerializer
+    
+# class JamDeleteView(DestroyAPIView):
+#     queryset = Jam.objects.all()
+#     serializer_class = JamSerializer        
